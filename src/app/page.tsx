09877b9902c22963823865cd6c2120f796d5ce46
Abhinav Goyal <abhinav621999goyal@gmail.com>
2024-04-02 02:35:33 +0530

    Assignment1-clone-wealthUp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,42 @@
-import Image from "next/image";
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <Head>
+        <title>Check Your Financial Health</title>
+      </Head>
+
+      <main className={styles.main}>
+        {/* Header */}
+        <h2 className={styles.title}>Check Your Financial Health</h2>
+        <p>Use weathometer to get free report card for your finances within minutes</p>
+        <div><Link href="/get-started" className={styles.button}> Get Started </Link></div>
+        <br/><hr/>
+        {/* Middle */}
+        <div className={styles.middleParent}>
+          <span className={styles.middleChild}><p>Expected Retirement Age</p>
+          <p>Identify Mistakes</p></span>
+          <span className={styles.middleChildImg}><img src="/image.jpg" alt="Financial Health Image" /></span>
+          <span className={styles.middleChild}>
+            <p>Personalised Road Map</p>
+            <p>Tips to Improve</p>
+          </span>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {/* Footer */}
+        <hr/> <br/>
+        <h3 className={styles.footerHeading}>How it works ?</h3>
+        <div className={styles.footerSection}>
+          <span>Answer Few questions</span> ---------------
+          <span>Register using phone and OTP </span> ---------------
+          <span>Get Report and personal roadmap </span>
+        </div>
+        <div><Link href="/get-started" className={styles.button}> Get Started </Link></div>
+        <br/>
+      </main>  
+    </div>
   );
 }
